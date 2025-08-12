73d7876e6a696b4df0409da49657d77bba758363
William Firmino <willian.firmino1@gmail.com>
2025-08-12 16:55:05 -0300

    Added server error log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -61,9 +61,15 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
-	logger.Info("Starting server", "addr", *addr)
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
 
-	err = http.ListenAndServe(*addr, app.routes())
+	logger.Info("Starting server", "addr", srv.Addr)
+
+	err = srv.ListenAndServe()
 
 	logger.Error(err.Error())
 	os.Exit(1)
